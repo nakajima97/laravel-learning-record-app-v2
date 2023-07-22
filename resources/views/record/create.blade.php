359064f24eda42b642ffacd1409b45853769ec51
Yudai Nakajima <kituneudon97@gmail.com>
2023-07-22 11:57:27 +0900

    #20 リファクタリング

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -5,7 +5,7 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12" x-data="{ showModal: false }">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
@@ -48,7 +48,11 @@
                                             </div>
                                         </div>
                                         <div class="p-2 w-full">
-                                            <x-stopwatch.modal />
+                                            <button @click="showModal = true"
+                                            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                            type="button">
+                                            ストップウォッチを表示する
+                                        </button>
                                         </div>
                                         <div class="p-2 w-full">
                                             <button
@@ -62,5 +66,6 @@
                 </div>
             </div>
         </div>
+        <x-stopwatch.modal />
     </div>
 </x-app-layout>
