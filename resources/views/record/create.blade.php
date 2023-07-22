459937154e8588bdcf10cd37a84f4e6a54ebc7e6
Yudai Nakajima <kituneudon97@gmail.com>
2023-07-22 13:03:54 +0900

    #20 新規追加と同じボタンを左右中央に表示

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -49,7 +49,7 @@
                                         </div>
                                         <div class="p-2 w-full">
                                             <button @click="showModal = true"
-                                            class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                            class="flex mx-auto text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                             type="button">
                                             ストップウォッチを表示する
                                         </button>
