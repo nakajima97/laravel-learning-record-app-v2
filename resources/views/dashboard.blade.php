f572cc48f0f08e194b065976485aefb3b58b3960
Nakajima <kituneudon97@gmail.com>
2023-07-29 15:12:04 +0900

    #51 ボタンクリック時に学習記録をつけるに遷移しないバグを修正 (#52)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,8 +11,9 @@
                 <div class="p-6 pb-0 text-gray-900 dark:text-gray-100">
                     <section class="text-gray-400 bg-gray-900 body-font overflow-hidden">
                         <div class="p-4 w-full">
-                            <button
-                                class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">学習記録を付ける</button>
+                            <a
+                                href="{{ route('categories.create') }}"
+                                class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">学習記録を付ける</a>
                         </div>
                     </section>
                 </div>
