875e3ad6b88453996940d927536a242b9c6a63ca
nakajima97 <kituneudon97@gmail.com>
2023-08-04 08:43:28 +0900

    #43 カテゴリー追加画面へのリンクを追加

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,23 +1,31 @@
 <x-app-layout>
-  <x-slot name="header">
-      <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-          カテゴリー一覧
-      </h2>
-  </x-slot>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            カテゴリー一覧
+        </h2>
+    </x-slot>
 
-  <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-          <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-              <div class="p-6 text-gray-900 dark:text-gray-100">
-                {{-- カードで表示 --}}
-                @foreach ($categories as $category)
-                  <div class="mb-2">
-                    <x-category.category-item :name="$category->name"/>
-                  </div>
-                @endforeach
-                {{-- ここまで --}}
-              </div>
-          </div>
-      </div>
-  </div>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <section class="text-gray-400 bg-gray-900 body-font overflow-hidden p-6">
+                        <div class="p-4 w-full">
+                            <a href="{{ route('categories.create') }}"
+                                class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">カテゴリー追加</a>
+                        </div>
+                    </section>
+                    <section class="text-gray-400 bg-gray-900 body-font overflow-hidden p-6">
+                        {{-- カードで表示 --}}
+                        @foreach ($categories as $category)
+                            <div class="mb-2">
+                                <x-category.category-item :name="$category->name" />
+                            </div>
+                        @endforeach
+                        {{-- ここまで --}}
+                    </section>
+                </div>
+            </div>
+        </div>
+    </div>
 </x-app-layout>
