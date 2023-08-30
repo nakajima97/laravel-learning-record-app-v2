bfe5085513d33a09decc0fd6ebc0088c87b03a09
nakajima97 <kituneudon97@gmail.com>
2023-08-30 21:33:20 +0900

    #47 lightテーマの時は白を中心の配色にするようにした

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 pb-0 text-gray-900 dark:text-gray-100">
-                    <section class="text-gray-400 bg-gray-900 body-font overflow-hidden">
+                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden">
                         <div class="p-4 w-full">
                             <a
                                 href="{{ route('records.create') }}"
@@ -18,9 +18,9 @@
                     </section>
                 </div>
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <section class="text-gray-400 bg-gray-900 body-font overflow-hidden">
+                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden">
                         <div class="container px-5 py-6 mx-auto">
-                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
+                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 dark:text-white">
                                 今月の総学習時間：
                                 @if ($total_study_time_this_month >= 60)
                                     {{ floor($total_study_time_this_month / 60) }}時間
@@ -29,9 +29,9 @@
                             </h1>
                         </div>
                     </section>
-                    <section class="text-gray-400 bg-gray-900 body-font overflow-hidden">
+                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden">
                         <div class="container px-5 py-6 mx-auto">
-                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">
+                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 dark:text-white">
                                 今日の総学習時間：
                                 @if ($total_study_time_today >= 60)
                                     {{ floor($total_study_time_today / 60) }}時間
@@ -40,14 +40,14 @@
                             </h1>
                         </div>
                     </section>
-                    <section class="text-gray-400 bg-gray-900 body-font overflow-hidden">
+                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden">
                         <div class="container px-5 py-6 mx-auto">
-                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 text-white">今日の学習記録</h1>
+                            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-2 dark:text-white">今日の学習記録</h1>
                             @foreach ($today_records as $today_record)
                                 <div class="-my-8 divide-y-2 divide-gray-800">
                                     <div class="py-8 flex flex-wrap md:flex-nowrap">
                                         <div class="md:flex-grow">
-                                            <h2 class="text-2xl font-medium text-white title-font mb-2">
+                                            <h2 class="text-2xl font-medium dark:text-white title-font mb-2">
                                                 {{ $today_record->category->name }}</h2>
                                             <p class="leading-relaxed">{{ $today_record->created_at }}</p>
                                             <p class="leading-relaxed">時間：{{ $today_record->minute }}分</p>
