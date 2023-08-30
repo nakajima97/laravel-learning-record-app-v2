579fa4f06271ea7357c5b7340fc65ff12ea66fe4
nakajima97 <kituneudon97@gmail.com>
2023-08-30 21:46:23 +0900

    #47 lightテーマに対応させた

--- resources/views/record/create.blade.php
+++ resources/views/record/create.blade.php
@@ -9,7 +9,7 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <section class="text-gray-400 bg-gray-900 body-font relative">
+                    <section class="text-gray-600  dark:text-gray-400 dark:bg-gray-900 body-font relative">
                         <form action="{{ route('records.store') }}" method="POST">
                             @csrf
                             <div class="container px-5 py-24 mx-auto">
@@ -20,7 +20,7 @@
                                                 <label for="category_id"
                                                     class="leading-7 text-sm text-gray-400">カテゴリー</label>
                                                 <select id="category_id" name="category_id"
-                                                    class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                    class="w-full dark:bg-gray-800 bg-opacity-40 rounded border dark:border-gray-700 focus:border-indigo-500 dark:focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none dark:text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                     @foreach ($records as $record)
                                                         <option value="{{ $record->id }}">{{ $record->name }}</option>
                                                     @endforeach
@@ -33,7 +33,7 @@
                                                 <label for="minute" class="leading-7 text-sm text-gray-400">時間</label>
                                                 <div class="w-full flex items-center gap-2">
                                                     <input type="number" id="minute" name="minute"
-                                                        class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
+                                                        class="w-full dark:bg-gray-800 bg-opacity-40 rounded border dark:border-gray-700 dark:focus:border-indigo-500 dark:focus:bg-gray-900 dark:focus:ring-2 focus:ring-indigo-900 text-base outline-none dark:text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                                     <p>分</p>
                                                 </div>
                                                 <x-input-error :messages="$errors->get('minute')" class="mt-2" />
@@ -43,7 +43,7 @@
                                             <div class="relative">
                                                 <label for="note" class="leading-7 text-sm text-gray-400">メモ</label>
                                                 <textarea id="note" name="note"
-                                                    class="w-full bg-gray-800 bg-opacity-40 rounded border border-gray-700 focus:border-indigo-500 focus:bg-gray-900 focus:ring-2 focus:ring-indigo-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
+                                                    class="w-full dark:bg-gray-800 bg-opacity-40 rounded border dark:border-gray-700 dark:focus:border-indigo-500 dark:focus:bg-gray-900 focus:ring-2 dark:focus:ring-indigo-900 h-32 text-base outline-none dark:text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                                 <x-input-error :messages="$errors->get('note')" class="mt-2" />
                                             </div>
                                         </div>
