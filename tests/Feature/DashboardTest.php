51437b1b73c2c4d5b01b9f9c303a1da82ad9fae9
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-02 15:45:00 +0900

    #6 値の正当性は確認していないがダッシュボードが開くことのテストコードを作成

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Feature;
 
+use App\Models\Category;
+use App\Models\Record;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
@@ -26,6 +28,19 @@ class DashboardTest extends TestCase
     public function test_ログイン状態ではダッシュボードを開くことができる(): void
     {
         $user = User::factory()->create();
+
+        $response = $this->actingAs($user)
+            ->get('/dashboard');
+
+        $response->assertStatus(200);
+    }
+
+    public function test_今日の学習記録一覧が表示されている(): void
+    {
+        $user = User::factory()->create();
+
+        Category::factory()->count(3)->create();
+        Record::factory()->count(5)->create();
 
         $response = $this->actingAs($user)
             ->get('/dashboard');
