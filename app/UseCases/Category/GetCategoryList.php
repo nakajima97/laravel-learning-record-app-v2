ff77f922f7c92d79ca7c20813cc27cb6e0d204f1
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-02 13:36:05 +0900

    #60 フォーマットの実行

--- app/UseCases/Category/GetCategoryList.php
+++ app/UseCases/Category/GetCategoryList.php
@@ -7,7 +7,7 @@ use App\Models\Category;
 class GetCategoryList
 {
     /**
-     * @param int|string|null $user_id 
+     * @param int|string|null $user_id
      * @return \Illuminate\Database\Eloquent\Collection<int, \App\Models\Category>
      */
     public function __invoke($user_id)
