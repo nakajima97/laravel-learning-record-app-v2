51913ed412fcc1ec19999595f1ad0371d9a06296
Yudai Nakajima <kituneudon97@gmail.com>
2023-09-02 17:12:56 +0900

    ダッシュボードのテストコードを作成

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -18,7 +18,7 @@ class CategoryFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'name' => fake()->text(),
             'user_id' => User::inRandomOrder()->first()->id
         ];
     }
--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use App\Models\Category;
 use App\Models\Record;
 use App\Models\User;
+use Carbon\Carbon;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -39,12 +40,75 @@ class DashboardTest extends TestCase
     {
         $user = User::factory()->create();
 
-        Category::factory()->count(3)->create();
-        Record::factory()->count(5)->create();
+        [$categories, $records] = $this->createCategoryAndRecord($user->id);
 
         $response = $this->actingAs($user)
             ->get('/dashboard');
 
         $response->assertStatus(200);
+
+        foreach($records as $record) {
+            $response->assertSeeText($record->category->name);
+            $response->assertSeeText($record->minute);
+            $response->assertSeeText($record->note);
+        }
+    }
+
+    public function test_今日の総学習時間が表示されている(): void
+    {
+        $user = User::factory()->create();
+
+        [$categories, $records] = $this->createCategoryAndRecord($user->id);
+        $day = Carbon::now()->day() === 1 ? Carbon::tomorrow() : Carbon::yesterday();
+        $yesterday_record = Record::factory()->create([
+            'user_id' => $user->id,
+            'created_at' => $day
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get('/dashboard');
+
+        $response->assertStatus(200);
+
+        $total_minute = $records->sum('minute');
+
+        $response->assertSeeText(floor($total_minute / 60) . "時間");
+        $response->assertSeeText($total_minute % 60 . "分");
+    }
+
+    public function test_今月の総学習時間が表示されている(): void
+    {
+        $user = User::factory()->create();
+
+        [$categories, $records] = $this->createCategoryAndRecord($user->id);
+        $day = Carbon::now()->day() === 1 ? Carbon::tomorrow() : Carbon::yesterday();
+        $yesterday_record = Record::factory()->create([
+            'user_id' => $user->id,
+            'created_at' => $day
+        ]);
+
+        $response = $this->actingAs($user)
+            ->get('/dashboard');
+
+        $response->assertStatus(200);
+
+        $total_minute = $records->sum('minute') + $yesterday_record->minute;
+
+        $response->assertSeeText(floor($total_minute / 60) . "時間");
+        $response->assertSeeText($total_minute % 60 . "分");
+    }
+
+    /**
+     * categoriesとrecordsにデータを追加する
+     * @param int|string|null $user_id
+     */
+    private function createCategoryAndRecord($user_id)
+    {
+        $categories = Category::factory()->count(3)->create();
+        $records = Record::factory()->count(5)->create([
+            'user_id' => $user_id
+        ]);
+
+        return [$categories, $records];
     }
 }
