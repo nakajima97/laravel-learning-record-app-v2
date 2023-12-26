d848e4933d2a3d5407f8a54f00a27856b9f282c0
nakajima97 <kituneudon97@gmail.com>
2023-12-26 21:35:00 +0900

    #86 タブクリックによりスタイルが変わるようにした

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -20,22 +20,26 @@
                             class="flex flex-wrap text-sm font-medium text-center text-gray-500 border-b border-gray-200 dark:border-gray-700 dark:text-gray-400">
                             <li class="me-2">
                                 <a href="#" aria-current="page" @click="open = 1"
-                                    class="inline-block p-4 text-blue-600 bg-gray-100 rounded-t-lg active dark:bg-gray-800 dark:text-blue-500">一覧</a>
+                                    class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+                                    x-bind:class="{ 'text-blue-600 bg-gray-100 dark:bg-gray-800 dark:text-blue-500': open === 1 }">一覧</a>
                             </li>
                             <li class="me-2">
                                 <a href="#" @click="open = 2"
-                                    class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300">アーカイブ済み</a>
+                                    class="inline-block p-4 rounded-t-lg hover:text-gray-600 hover:bg-gray-50 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+                                    x-bind:class="{ 'text-blue-600 bg-gray-100 dark:bg-gray-800 dark:text-blue-500': open === 2 }">アーカイブ済み</a>
                             </li>
                         </ul>
                     </section>
-                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden p-6" x-show='open === 1'>
+                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden p-6"
+                        x-show='open === 1'>
                         @foreach ($categories as $category)
                             <div class="mb-2">
                                 <x-category.category-item :name="$category->name" :id="$category->id" />
                             </div>
                         @endforeach
                     </section>
-                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden p-6" x-show='open === 2'>
+                    <section class="bg-gray-100 dark:text-gray-400 dark:bg-gray-900 body-font overflow-hidden p-6"
+                        x-show='open === 2'>
                         アーカイブ済み一覧
                     </section>
                 </div>
