c0552c81d7343b66d89e0baf18170efa3c6d4dec
Yudai Nakajima <kituneudon97@gmail.com>
2024-01-13 15:49:00 +0900

    #83 コメントの追加

--- resources/js/drag-move.js
+++ resources/js/drag-move.js
@@ -1,22 +1,29 @@
+// ドラッグ開始時
 const onDragStart = (event) => {
     event.dataTransfer.setData("text/plain", event.target.id);
     event.dataTransfer.dropEffect = "move";
 };
 
+// ドロップしたとき
 const onDrop = (event) => {
     event.currentTarget.after(
         document.getElementById(event.dataTransfer.getData("text"))
     );
 };
 
+// ドロップ対象に入ったとき
 const onDragEnter = (event) => {
+    // デフォルトで動作するイベントのキャンセル
     event.preventDefault();
 };
 
+// ドロップ対象を離れたとき
 const onDragOver = (event) => {
+    // デフォルトで動作するイベントのキャンセル
     event.preventDefault();
 };
 
+// イベントリスナーの追加
 document.querySelectorAll(".drag-target").forEach((element) => {
     element.addEventListener("dragstart", onDragStart);
     element.addEventListener("drop", onDrop);
