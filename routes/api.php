5a0caa1fb219fd6a01e7b2e1e912cb56614088a3
Yudai Nakajima <kituneudon97@gmail.com>
2024-01-16 16:20:24 +0900

    #83 php cs fixerの実行

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryOrderController;
 
-
 /*
 |--------------------------------------------------------------------------
 | API Routes
