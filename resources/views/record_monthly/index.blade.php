995b246290c433ec5f246f725e72f1779a32388c
Nakajima <kituneudon97@gmail.com>
2024-02-27 08:54:40 +0900

    #134 テーブルを使って総学習時間を表示するようにした (#135)

--- resources/views/record_monthly/index.blade.php
+++ resources/views/record_monthly/index.blade.php
@@ -9,13 +9,22 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
               <div class="p-6 text-gray-900 dark:text-gray-100">
-                <ul>
-                  @foreach ($monthly_records as $record)
-                    <li>
-                      {{ $record->year }}年{{ $record->month }}月 {{ floor($record->total_time / 60) }}時間{{ $record->total_time % 60 }}分
-                    </li>
-                  @endforeach
-                </ul>
+                <table class="table-auto">
+                  <thead>
+                    <tr>
+                      <th class="px-4 py-2">年月</th>
+                      <th class="px-4 py-2">学習時間</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    @foreach ($monthly_records as $record)
+                      <tr>
+                        <td class="border px-4 py-2">{{ $record->year }}年{{ $record->month }}月</td>
+                        <td class="border px-4 py-2">{{ floor($record->total_time / 60) }}時間{{ $record->total_time % 60 }}分</td>
+                      </tr>
+                    @endforeach
+                  </tbody>
+                </table>
                 {{ $monthly_records->links() }}
               </div>
             </div>
