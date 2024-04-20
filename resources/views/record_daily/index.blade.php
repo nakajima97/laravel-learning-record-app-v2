a56187f15f0f116c673d5093b2d64d12346b80ed
Yudai Nakajima <kituneudon97@gmail.com>
2024-04-20 16:00:51 +0900

    #137 細かい見た目の調整

--- resources/views/record_daily/index.blade.php
+++ resources/views/record_daily/index.blade.php
@@ -3,7 +3,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-            1週間の学習時間
+            直近7日間の学習時間
         </h2>
     </x-slot>
 
@@ -11,13 +11,6 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    <div>
-                        1週間の学習時間を表示します
-                        @foreach ($daily_records as $daily_record)
-                            {{ $daily_record->month }}月{{ $daily_record->day }}日:
-                            {{ floor($daily_record->total_time / 60) }}時間{{ $daily_record->total_time % 60 }}分<br>
-                        @endforeach
-                    </div>
                     <div>
                         <canvas id="myChart"></canvas>
                         <script>
@@ -29,7 +22,7 @@
                                         return '"' . $daily_record->month . '月' . $daily_record->day . '日' . '"';
                                     })->implode(",") !!}],
                                     datasets: [{
-                                        label: '時間(分)',
+                                        label: '分',
                                         data: [{{ $daily_records->implode('total_time', ',') }}],
                                         borderWidth: 1
                                     }]
