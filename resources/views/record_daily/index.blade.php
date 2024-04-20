3390d994638de6c5d6da1e1b88f8ca5f2a11e808
Yudai Nakajima <kituneudon97@gmail.com>
2024-04-20 15:41:21 +0900

    #137 chart.jsのサンプルのグラフを表示した

--- resources/views/record_daily/index.blade.php
+++ resources/views/record_daily/index.blade.php
@@ -1,3 +1,5 @@
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
@@ -9,10 +11,37 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
-                    1週間の学習時間を表示します
-                    @foreach ($daily_records as $daily_record)
-                    {{ $daily_record->month }}月{{ $daily_record->day }}日: {{ floor($daily_record->total_time / 60) }}時間{{ $daily_record->total_time % 60 }}分<br>
-                    @endforeach
+                    <div>
+                        1週間の学習時間を表示します
+                        @foreach ($daily_records as $daily_record)
+                            {{ $daily_record->month }}月{{ $daily_record->day }}日:
+                            {{ floor($daily_record->total_time / 60) }}時間{{ $daily_record->total_time % 60 }}分<br>
+                        @endforeach
+                    </div>
+                    <div>
+                        <canvas id="myChart"></canvas>
+                        <script type="module">
+                            var ctx = document.getElementById("myChart");
+                            new Chart(ctx, {
+                                type: 'bar',
+                                data: {
+                                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                                    datasets: [{
+                                        label: '# of Votes',
+                                        data: [12, 19, 3, 5, 2, 3],
+                                        borderWidth: 1
+                                    }]
+                                },
+                                options: {
+                                    scales: {
+                                        y: {
+                                            beginAtZero: true
+                                        }
+                                    }
+                                }
+                            });
+                        </script>
+                    </div>
                 </div>
             </div>
         </div>
