197a50c6b3804f8ef5060c8f26c92361d0012859
Yudai Nakajima <kituneudon97@gmail.com>
2024-05-03 14:45:51 +0900

    #153 テストのしやすさを上げるためにカテゴリーの数を増やした

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -14,10 +14,34 @@ class CategorySeeder extends Seeder
     public function run(): void
     {
         DB::table('categories')->insert([
-            'name' => 'プログラミング'
+            'name' => '読書'
         ]);
         DB::table('categories')->insert([
-            'name' => '読書'
+            'name' => '英語'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Laravel'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'React'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Fast API'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Ruby on Rails'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Go'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Cake PHP'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Vue.js'
+        ]);
+        DB::table('categories')->insert([
+            'name' => 'Django'
         ]);
     }
 }
